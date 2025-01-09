793439a9cf9ab527beebedb20d6d4669b5f3ea32
Scholz-Flo <florian.scholz@advastore.com>
2025-01-10 00:20:29 +0100

    add sounds to player

--- GoateiCounter/Controllers/AudioController.cs
+++ GoateiCounter/Controllers/AudioController.cs
@@ -5,18 +5,18 @@ namespace GoateiCounter.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class VideoController(IWebHostEnvironment webHostEnvironment) : ControllerBase
+public class AudioController(IWebHostEnvironment webHostEnvironment) : ControllerBase
 {
     private readonly IWebHostEnvironment _webHostEnvironment = webHostEnvironment;
 
     [HttpGet()]
     public IActionResult GetByIndex(int index)
     {
-        var folder = Path.Combine(_webHostEnvironment.WebRootPath, "videos");
+        var folder = Path.Combine(_webHostEnvironment.WebRootPath, "sounds");
         var files = Directory.GetFiles(folder);
 
-        Response.ContentType = new MediaTypeHeaderValue("video/mp4").ToString();// Content type
+        Response.ContentType = new MediaTypeHeaderValue("audio/mp3").ToString();// Content type
         FileStream filestream = System.IO.File.OpenRead(files[index]);
-        return File(filestream, contentType: "video/mp4", fileDownloadName: "video", enableRangeProcessing: true);
+        return File(filestream, contentType: "audio/mp3", fileDownloadName: "audio", enableRangeProcessing: true);
     }
 }
